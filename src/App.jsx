a9a745237733d29125974e1b2abb0ed68569fc87
tushargupta8954 <tushargupta8954@gmail.com>
2025-10-08 23:07:41 +0530

    Created the Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Header from './components/Header'
+import Navbar from './components/Navbar/Navbar'
 
 const App = () => {
   return (
     <div>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-        </Routes>
-      </Router>
+      
+      <Navbar />
     </div>
   )
 }
