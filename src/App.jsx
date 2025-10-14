dff137ddf71fab36b45476a165c3b8ee01db0d6e
tushargupta8954 <tushargupta8954@gmail.com>
2025-10-14 16:05:12 +0530

    Created the title

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import Program from './components/Program/Program'
+import Title from './components/Title/Title'
 
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <div className="container">
+        <Title subTitle='Our program' title='What We Offer' />
         <Program />
       </div>
        </div>
